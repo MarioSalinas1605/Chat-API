65c51f28df94d7eb4240dbffb660d3b8c6ee91ed
marioalejandro.cortez <marioalejandro.cortez@bbva.com>
2020-04-22 08:17:12 -0500

    feature/ reading query and body from request

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const router = express.Router()
 
 const app = express()
 
-app.use('/', (request, response) => {
-    response.send('Hi')
+app.use(express.json())
+app.use(router)
+
+router.get('/', (req, res) => {
+    res.send('Hi from get')
+})
+
+router.post('/', (req, res) => {
+    console.log(req.query);
+    console.log(req.body);
+    res.send('Hi from post')
 })
 
 app.listen(3000, () => {
