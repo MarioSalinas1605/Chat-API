93a0673ed7ffc6fcae5c239f0fa72053c5868e61
Mario Salinas <mario_1605@live.com.mx>
2020-04-27 07:25:38 -0500

    feature/writing and reading data from Atlas Mongo DB

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,20 @@
-const list = []
+const db = require('mongoose')
+const Model = require('./model')
+db.Promise = global.Promise
+const mongoURI = 'mongodb+srv://admin:4321@cluster0-mxwrf.mongodb.net/telegrom?retryWrites=true&w=majority'
+db.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+console.log(`[db] Successful connection`);
+
 
 function addMessage(message) {
-    list.push(message)
+    console.log('[store] Saving message');
+    const myMessage = new Model(message)
+    myMessage.save()
 }
 
-function getMessages() {
-    return list
+async function getMessages() {
+    const messages = await Model.find()
+    return messages
 }
 
 module.exports = {
