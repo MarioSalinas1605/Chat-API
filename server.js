ae30b97eb09dae94cda18cc9d4a5a97f57bb1186
Mario Salinas <mario_1605@live.com.mx>
2020-05-25 14:50:15 -0500

    feature/basic socket of messages

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+
 const routes = require('./network/routes')
 const db = require('./db')
-const app = express()
+const socket = require('./socket')
+
 
 db('mongodb+srv://admin:4321@cluster0-mxwrf.mongodb.net/telegrom?retryWrites=true&w=majority')
+
 app.use(express.json())
+socket.connect(server)
+
 routes(app)
 
 app.use('/app', express.static('public'))
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
 })
